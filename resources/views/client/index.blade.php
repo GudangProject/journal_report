fd0d8f8dc5fe07aeab67dfcd3dc9ae7a670c49d3
jarwonozt <jarwonoztaveiro@gmail.com>
2022-08-24 14:52:37 +0700

    Artikel diganti Fanpage

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -20,7 +20,7 @@
                     @include('client.widget.slide-small', ['title'=>'INFOGRAFIS', 'data'=>$infografis, 'limit'=>4])
                     <hr>
                     @include('client.widget.list', ['title'=>'INFO KEPEGAWAIAN', 'data'=> $data_kepegawaian, 'category_id' => 3, 'limit'=>2])
-                    @include('client.widget.list-image', ['data'=> $wilayah, 'title'=> "ARTIKEL", 'limit'=>3])
+                    @include('client.widget.embed', ['data'=> $files, 'title'=> "FANSPAGE"])
                     </div>
                 </div>
             </div>
