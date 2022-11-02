7e3d7f87d392ffd8e517eb165b8d060ca8ad39d0
Jarwonozt <67833034+jarwonozt@users.noreply.github.com>
2022-11-02 10:29:41 +0700

    change title profile video kanwil kemenag sulbar

--- resources/views/client/widget/single-video-custom.blade.php
+++ resources/views/client/widget/single-video-custom.blade.php
@@ -9,7 +9,7 @@
     <div class="col-12 d-flex justify-content-center">
         <div class="bg-video">
             <div class="video-card">
-                <span class="video-title"><strong>Profil Sulbar Kemenag</strong></span>
+                <span class="video-title"><strong>Profil Kanwil Kemenag Sulbar</strong></span>
                 <a data-fancybox="video-gallery" href="https://www.youtube.com/watch?v={{ $data[0]->youtube_id }}">
                     <img src="https://img.youtube.com/vi/{{ $data[0]->youtube_id }}/maxresdefault.jpg" class="video-thumbnail" alt="">
                     <div class="video-overlay">
