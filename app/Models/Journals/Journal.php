ed79a96d25dd8652805f3df5444032f9a040cd69
jarwonozt <jarwonoztaveiro@gmail.com>
2022-11-20 12:02:46 +0700

    Database : create table journal and payments

---
+++ app/Models/Journals/Journal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Journals;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Journal extends Model
+{
+    use HasFactory;
+
+    protected $table = 'journals';
+    protected $keyPrimary = 'id';
+
+}
---
+++ database/migrations/2022_11_20_115324_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('rumpun_id')->nullable();
+            $table->bigInteger('journal_id')->nullable();
+            // $table->bigInteger('journal_id')->nullable();
+            $table->string('nama')->nullable();
+            $table->string('judul_naskah')->nullable();
+            $table->string('link_naskah')->nullable();
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
