9d986b66fc6b41bab39ed48d9a0daf1738ec6619
jarwonozt <jarwonoztaveiro@gmail.com>
2023-01-31 19:51:00 +0700

    API: update remote api

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>404 Not Found</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container mt-5 pt-5">
-        <div class="alert alert-danger text-center">
-            <h2 class="display-3">404</h2>
-            <p class="display-5">Oops! Something is wrong.</p>
+<html lang="en">
+
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>404 Error Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+
+    <body>
+        @include('sweetalert::alert')
+
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The page you’re looking for doesn’t exist.
+                  </p>
+                <a href="https://wa.me/+6282145095183?text=Halo%20saya%20ingin%20mengaktifkan%20website%20{{ url()->current() }}" class="btn btn-success">Contact CS</a>
+                <a href="/logout" class="btn btn-primary">Go Home</a>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+
+
 </html>
