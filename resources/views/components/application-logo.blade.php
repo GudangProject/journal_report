af7333723be2496dadfbff3f5eeecf4d1e5b28e5
jarwonozt <jarwonoztaveiro@gmail.com>
2023-02-05 14:13:36 +0700

    update logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -6,6 +6,6 @@
 @endphp
 <span class="brand-logo text-xl">
     @if (isset($logo->logo))
-        <img src="{{ asset('storage') }}/assets/{{ $logo->logo }}" id="blog-feature-image" class="rounded mr-2 mb-1 mb-md-0 bg-secondary" alt="Blog Featured Image" />
+        <img src="{{ asset('storage') }}/assets/{{ $logo->logo }}" id="blog-feature-image" class="rounded mr-2 mb-1 mb-md-0 bg-secondary" height="50" width="180" alt="Logo" />
     @endif
 </span>
